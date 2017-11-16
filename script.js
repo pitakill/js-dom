ee9c6c5182a630e40cd36794e37a515ece596cc9
Lepoldo Caballero <git@pitakill.net>
2017-11-16 12:29:39 -0600

    Add Button to dispatch the print of the text

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Traemos todos los botones del documento
+  var button = document.getElementsByTagName('button');
+  // Referenciamos al primero, porque sabemos que es el único
+  var show = button[0];
+  // Agregamos un receptor de click con su respectivo callback
+  show.addEventListener('click', function() {
+    print();
+  });
+});
+
+function print() {
   var root = document.getElementById('root');
   root.innerHTML = '<h1>Hola</h1>';
-});
+};
